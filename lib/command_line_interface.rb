a660f92d82a9a0b84069954cde476fc0986ac8f5
Bri Fetterley <bnfetterley@usfca.edu>
2019-10-21 15:52:25 -0400

    get character from user method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,10 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  
 end
 
 def get_character_from_user
   puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  foo = STDIN.gets.chomp
+  puts foo
 end
-
-puts "Hello World"
